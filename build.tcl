f2025070f3b6cd37494e8a5ddf8c66f175a14db3
Danilo Chang <ray2501@gmail.com>
2021-01-18 09:42:24 +0800

    Update to 4.99.20

--- build.tcl
+++ build.tcl
@@ -1,8 +1,8 @@
 #!/usr/bin/tclsh
 
 set arch "x86_64"
-set base "naviserver-4.99.19"
-set fileurl "https://sourceforge.net/projects/naviserver/files/naviserver/4.99.19/naviserver-4.99.19.tar.gz"
+set base "naviserver-4.99.20"
+set fileurl "https://sourceforge.net/projects/naviserver/files/naviserver/4.99.20/naviserver-4.99.20.tar.gz"
 
 set var [list wget $fileurl -O $base.tar.gz]
 exec >@stdout 2>@stderr {*}$var
